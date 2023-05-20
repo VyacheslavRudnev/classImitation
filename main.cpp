49c77fc71c459997b1721eed3010e16227314db5
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2023-05-20 17:20:45 +0300

    HW task 01 done

--- main.cpp
+++ main.cpp
@@ -3,13 +3,63 @@
 #include "parrot.h"
 #include "hamster.h"
 
+namespace dog
+{
+    void dog()
+    {
+		Dog* dog = new Dog();
+		dog->Type();
+		dog->Show();
+		dog->Sound();
+		cout << endl << endl;
+		delete dog;
+	}
+}
 
+namespace cat
+{
+    void cat()
+    {
+		Cat* cat = new Cat();
+		cat->Type();
+		cat->Show();
+		cat->Sound();
+		cout << endl << endl;
+		delete cat;
+	}
+}
+
+namespace parrot
+{
+    void parrot()
+    {
+		Parrot* parrot = new Parrot();
+		parrot->Type();
+		parrot->Show();
+		parrot->Sound();
+		cout << endl << endl;
+		delete parrot;
+	}
+}
+
+namespace hamster
+{
+    void hamster()
+    {
+		Hamster* hamster = new Hamster();
+		hamster->Type();
+		hamster->Show();
+		hamster->Sound();
+		cout << endl << endl;
+		delete hamster;
+	}
+}
 
 int main()
 {
     ukr_lang;
     
-    HomePets** pets = new HomePets * [4]
+   /* HomePets** pets = new HomePets * [4]
     {
         new Dog(),
         new Cat(),
@@ -28,7 +78,13 @@ int main()
     for (size_t i = 0; i < 2; i++)
     {
         delete pets[i];
-    }
+    }*/
+
+    dog::dog();
+    cat::cat();
+    parrot::parrot();
+    hamster::hamster();
+
 
     return 0;
 }
